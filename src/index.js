b42ce562b85d136b17a8598a2bc2a5d2714b138a
uttej <uttej.thoompally@gmail.com>
2019-07-17 00:23:23 -0700

    removing unwanted folder

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>Welcome to my website !!!</h1>;
+const element = <h1>Welcome to Uttej's website !!!</h1>;
 ReactDOM.render(element, document.getElementById("root"));
